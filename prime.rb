983ca0cb57de8b57e0b0722473e2a767ff620bdb
Zoey Chesny <z.chesny@gmail.com>
2020-04-09 22:45:16 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(num)
+  if num < 2
+    return false
+  end
+  (2..Integer.sqrt(num)).to_a.each do |divisor|
+    if num % divisor == 0
+      return false
+    end
+  end
+  return true
+end
